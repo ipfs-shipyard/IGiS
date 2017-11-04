a91a7015abc45bdf6865dfd0efef04aec7b1ad41
Łukasz Magiera <magik6k@gmail.com>
2017-11-04 19:19:37 +0100

    Clone window

--- src/main/scala/igis/App.scala
+++ src/main/scala/igis/App.scala
@@ -1,9 +1,9 @@
 package igis
 
-import binding.highlightjs.HighlightJS
 import igis.app.controllers.{BlobController, RepoController, TreeController}
 import igis.mvc.{Node, Request, Response, Router}
-import org.scalajs.dom.raw.HashChangeEvent
+import igis.util.Debug
+import org.scalajs.dom.raw.{HTMLElement, HashChangeEvent, MouseEvent}
 
 import scala.scalajs.js.JSApp
 import org.scalajs.dom.{document, window}
@@ -28,12 +28,31 @@ object App extends JSApp {
     router(new Request(location, node)).andThen {
       case Success(Response.DataResponse(result)) =>
         document.getElementById("body").innerHTML = result
+        initPopdown()
       case Success(Response.RedirectResponse(result)) =>
         window.location.hash = result
         updateLocation()
       case Failure(f) =>
         f.printStackTrace()
         document.getElementById("body").innerHTML = "error 212"
+    }
+  }
+
+  def initPopdown(): Unit = {
+    val elems = document.getElementsByClassName("popdown")
+
+    (0 until elems.length).map(elems.apply).foreach { elem =>
+      elem.asInstanceOf[HTMLElement].onclick = (e: MouseEvent) => {
+        if (e.button == 0) {
+          e.target match {
+            case elem: HTMLElement =>
+              if (elem.classList.contains("popdown")) {
+                elem.parentElement.nextElementSibling.asInstanceOf[HTMLElement].classList.toggle("hidden")
+              }
+            case _ =>
+          }
+        }
+      }
     }
   }
 
@@ -46,6 +65,8 @@ object App extends JSApp {
     window.onhashchange = {(_: HashChangeEvent) =>
       updateLocation()
     }
+
+
 
     node.init().andThen{
       case Success(_) =>
