ee615a542c34d1caaed1584649499b2f7e5b2d7b
Łukasz Magiera <magik6k@gmail.com>
2017-11-20 03:33:57 +0100

    Fix links in commit list

--- src/main/scala/igis/app/controllers/CommitsController.scala
+++ src/main/scala/igis/app/controllers/CommitsController.scala
@@ -1,6 +1,7 @@
 package igis.app.controllers
 
 import binding.git.GitCommit
+import eu.devtty.cid.CID
 import igis.mvc.{Controller, Node, Request, Response}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,13 +14,13 @@ import org.scalajs.dom.raw.HTMLLinkElement
 class CommitsController extends Controller {
   lazy val template = html.commits().template
 
-  def fetchCommits(rootCid: String, depth: Int, node: Node): Future[Seq[GitCommit]] = {
+  def fetchCommits(rootCid: String, depth: Int, node: Node): Future[Seq[(CID, GitCommit)]] = {
     node.ipfs.dag.get(rootCid).flatMap{ res =>
       val commit = res.value.asInstanceOf[GitCommit]
 
-      commit.parents.take(depth) //get at mont [depth] parents
+      commit.parents.take(depth) //get at most [depth] parents
         .map(p => fetchCommits(p.cid().toBaseEncodedString(), depth - commit.parents.length, node)) // fetch their subtrees
-        .fold(Future.successful(Seq(commit))){ case (a, b) => a.flatMap(as => b.map(bs => as ++ bs))} //metge subtrees
+        .fold(Future.successful(Seq((new CID(rootCid), commit)))){ case (a, b) => a.flatMap(as => b.map(bs => as ++ bs))} //merge subtrees
     }
   }
 
@@ -31,15 +32,15 @@ class CommitsController extends Controller {
 
     fetchCommits(at, 10, req.node).andThen{
       case Success(commits) =>
-        builder.setMultiple[GitCommit]("commits-root", commits, { case(b, commit) =>
+        builder.setMultiple[(CID, GitCommit)]("commits-root", commits, { case(b, (cid, commit)) =>
           b.set("commit-message", commit.message.lines.next())
           b.set("commit-author", commit.author.name)
           b.set("commit-date", TimeUtil.gitTimeToDate(commit.author.date).toDateString())
-          b.modElement("commit-link", _.asInstanceOf[HTMLLinkElement].href = s"#/commit/${req.remPath}")
+          b.modElement("commit-link", _.asInstanceOf[HTMLLinkElement].href = s"#/commit/${cid.toBaseEncodedString()}")
         })
 
         //TODO: this is terrible, will lose subtrees
-        builder.modElement("commit-older", _.asInstanceOf[HTMLLinkElement].href = s"#/repo/commits/$repo/${commits.last.parents(0).cid().toBaseEncodedString()}")
+        builder.modElement("commit-older", _.asInstanceOf[HTMLLinkElement].href = s"#/repo/commits/$repo/${commits.last._2.parents(0).cid().toBaseEncodedString()}")
     }
 
     Future.successful(Response.withElement(builder.element))
