a63ac3cfa7c692d11ede01148d09403e1fc09d79
Łukasz Magiera <magik6k@gmail.com>
2018-04-03 23:08:14 +0200

    fix getServedUrl in config

--- config/paths.js
+++ config/paths.js
@@ -32,10 +32,7 @@ const getPublicUrl = appPackageJson =>
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 function getServedPath(appPackageJson) {
-  const publicUrl = getPublicUrl(appPackageJson);
-  const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
-  return ensureSlash(servedUrl, true);
+  return '';
 }
 
 // config after eject: we're in ./config/
