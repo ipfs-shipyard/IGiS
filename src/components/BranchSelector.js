8114e3519fc2c20f5fb0bb2be96b2ccb7a4ea98a
Łukasz Magiera <magik6k@gmail.com>
2018-05-09 17:41:38 +0200

    Improve tag picker

--- src/components/BranchSelector.js
+++ src/components/BranchSelector.js
@@ -2,27 +2,67 @@ import React, { Component } from 'react'
 import Url from '../lib/Url'
 
 class BranchSelector extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {show: ''}
+
+    this.switchType = this.switchType.bind(this)
+  }
+
+  switchType(type) {
+    return () => this.setState({show: type})
+  }
+
   render() {
     if (!this.props.repo) return null
 
     return (
       <div className="BranchSelector">
-        <div className="title">Branch:</div>
-      	<select onChange={e => this.handleChange(e)} value={this.props.branch}>
-          {this.renderOptions(this.props.repo.branches)}
-        </select>
+        <div>Branch: <span className="branch" onClick={this.switchType('heads')}>{this.props.branch}</span></div>
+        {this.renderDropdown()}
       </div>
     )
   }
 
-  renderOptions(branches) {
-    return Object.keys(branches).sort((a, b) => this.compareBranches(a, b)).map(this.branchNick).map(b =>
-      <option key={b} value={b}>{b}</option>
-    )
+  renderDropdown() {
+    if(this.state.show === '') return
+
+    return <div className="branchDropdown bg-white ba">
+      <div>
+        <div className="bb pa1 refType">
+          {this.renderButton('heads', 'Branches')}
+          {this.renderButton('tags', 'Tags')}
+        </div>
+      </div>
+      <div className="branchList pa1">
+        {this.renderOptions(this.props.repo.branches)}
+      </div>
+    </div>
   }
 
-  handleChange(event) {
-    window.location.href = '#' + Url.toBranch(this.props.repo, event.target.value)
+  renderButton(type, name) {
+    return <span onClick={this.switchType(type)} className={type !== this.state.show ? 'selectable' : ''}>{name}</span>
+  }
+
+  renderOptions(branches) {
+    const renderType = (type) => {
+      return Object.keys(branches).filter(b => b.startsWith('refs/' + type + '/'))
+        .sort((a, b) => this.compareBranches(a, b))
+        .map(this.branchNick).map(b =>
+        <div><a href="#" onClick={this.handleChange(b)}>{b}</a></div>
+      )
+    }
+
+    switch(this.state.show) {
+      case 'heads':
+        return renderType('heads')
+      case 'tags':
+        return renderType('tags')
+    }
+  }
+
+  handleChange(event, to) {
+    return () => window.location.href = '#' + Url.toBranch(this.props.repo, to)
   }
 
   branchNick(branch) {
