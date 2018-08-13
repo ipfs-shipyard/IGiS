9f3b4136e21e767d6b4223d967a60fa889599f6a
Dirk McCormick <dirkmdev@gmail.com>
2018-08-13 17:20:33 -0400

    Fix display of long breadcrumbs

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -10,24 +10,35 @@ class BreadCrumb extends Component {
       return null
     }
 
+    // Shorten cid and don't display too many crumbs
+    const abbrev = this.props.crumbs.slice()
+    abbrev[0] = abbrev[0].substring(0, 8)
+    for (let i = 1; i < abbrev.length && abbrev.join('/').length > 60 && abbrev.filter(c => !!c).length > 3; i++) {
+      abbrev[i] = ''
+    }
+
     return (
-      <div className="BreadCrumb">{this.renderCrumbs(crumbs)}</div>
+      <div className="BreadCrumb">{this.renderCrumbs(crumbs, abbrev)}</div>
     )
   }
 
-  renderCrumbs(crumbs) {
-    return crumbs.map((c, i) => {
+  renderCrumbs(crumbs, abbrev) {
+    return abbrev.map((c, i) => {
       const filePath = [''].concat(crumbs.slice(1, i + 1)).join('/')
       const linkPath = Url.toBranch(this.props.repo, this.props.branch) + filePath
-      // Shorten cid
-      const name = i === 0 ? c.substring(0, 8) : c
+      if (!c) {
+        if (i > 1) {
+          return null
+        }
+        c = '…'
+      }
       return (
         <div key={i}>
           {i > 0 && <div className="separator">/</div>}
-          {i < crumbs.length - 1 ? (
-            <Link to={linkPath} className="crumb">{name}</Link>
+          {i < crumbs.length - 1 && c !== '…' ? (
+            <Link to={linkPath} className="crumb">{c}</Link>
           ) : (
-            <div className="crumb">{name}</div>
+            <div className="crumb">{c}</div>
           )}
         </div>
       )
